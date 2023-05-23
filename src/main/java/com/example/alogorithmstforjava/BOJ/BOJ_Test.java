58b8d51dc51b5071170c44f1156cd3c98b46205b
johanpark <pyhan23@gmail.com>
2023-05-23 15:09:09 +0900

    Test Git

--- src/main/java/com/example/alogorithmstforjava/BOJ/BOJ_Test.java
+++ src/main/java/com/example/alogorithmstforjava/BOJ/BOJ_Test.java
@@ -3,5 +3,6 @@ package com.example.alogorithmstforjava.BOJ;
 public class BOJ_Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Test Git");
     }
 }
