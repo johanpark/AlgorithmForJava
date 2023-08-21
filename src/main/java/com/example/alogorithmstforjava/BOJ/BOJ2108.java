241d663de3cf56ef2917ce758c47c3ebe9074c29
johanpark <pyhan23@gmail.com>
2023-08-21 16:38:49 +0900

    2023/08/21

--- src/main/java/com/example/alogorithmstforjava/BOJ/BOJ2108.java
+++ src/main/java/com/example/alogorithmstforjava/BOJ/BOJ2108.java
@@ -3,54 +3,54 @@ package com.example.alogorithmstforjava.BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class BOJ2108 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
 
-        int n = Integer.parseInt(st.nextToken());
-        List<Integer> target = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int s = Integer.parseInt(st.nextToken());
-            target.add(s);
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int[] frequency = new int[8001];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+            sum += nums[i];
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+            frequency[nums[i]+4000]++;
         }
-        List<Integer> collect = target.stream().sorted().collect(Collectors.toList());
+        br.close();
+
         //산술평균
-        int sum = collect.stream().mapToInt(Integer::intValue).sum() / target.size();
+        double mean = (double) sum / N;
+        System.out.println(Math.round(mean));
+
         //중앙값
-        int middle = collect.get(collect.size() / 2);
+        Arrays.sort(nums);
+        int median = nums[N / 2];
+        System.out.println(median);
+
         //최빈값
-        //범위
-        int max = collect.stream().mapToInt(x -> x).max().getAsInt();
-        int min = collect.stream().mapToInt(x -> x).min().getAsInt();
-        int range = max - min;
-
-
-        System.out.println(sum);
-        System.out.println(middle);
-        System.out.println(range);
-    }
-
-    private static int findDuplicate(List<Integer> collect) {
-        Integer tortoise = collect.get(0);
-        Integer hare = collect.get(0);
-        do {
-            tortoise = collect.get(tortoise);
-            hare = collect.get(collect.get(hare));
-        } while (!Objects.equals(tortoise, hare));
-        Integer ptr1 = collect.get(0);
-        Integer ptr2 = tortoise;
-        while(!Objects.equals(ptr1, ptr2)) {
-            ptr1 = collect.get(ptr1);
-            ptr2 = collect.get(ptr2);
+        int maxFrequency = Arrays.stream(frequency).max().getAsInt();
+        ArrayList<Integer> target = new ArrayList<>();
+        for (int i = 0; i < frequency.length; i++) {
+            if(frequency[i] == maxFrequency) {
+                target.add(i - 4000);
+            }
         }
-        return ptr1;
+        Collections.sort(target);
+        Integer result = target.get(0);
+        if(target.size() > 1) {
+            result = target.get(1);
+        }
+        System.out.println(result);
+
+        //범위
+        int range = max - min;
+        System.out.println(range);
     }
 }
